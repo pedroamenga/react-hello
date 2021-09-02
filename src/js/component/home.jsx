f61a16acbb0daaa57370c6229207a82935685f85
pedroamenga <pedroam92@gmail.com>
2021-09-02 18:29:15 +0000

    landingpagefaltaprops

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,31 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Jumbotron } from "./jumbotron";
+import { Navbar } from "./navbar";
+import { Cards } from "./cards";
 
 //create your first component
 const Home = () => {
+	let variacioncartas = {
+		var1: "variacion1",
+		var2: "variacion2",
+		var3: "variacion3"
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<Navbar titulo="Mi NavBar" />
+			</div>
+			<div className="container-fluid">
+				<div className="JT">
+					<Jumbotron />
+				</div>
+				<div className="cards">
+					<Cards />
+				</div>
+			</div>
+		</>
 	);
 };
 
---
+++ src/js/component/navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Navbar = props => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Hola
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Features
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Pricing
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
